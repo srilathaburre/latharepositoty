eec2bea9cccf8b17f9172d9cdb2049408a44224e
srilathaburre <136457346+srilathaburre@users.noreply.github.com>
2023-07-07 14:39:02 -0400

    added second heading

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>This heading is added from Srilatha local repository</h1>
+    <h1>This heading is added by some other developer and pushed</h1>
     </div>
   );
 }
